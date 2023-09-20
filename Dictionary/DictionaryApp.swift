b88a86907d6946dbd3065c52931fc5709fe0fca5
Aleksey Ryabchenko <103571540+AlekseyRyabchenko@users.noreply.github.com>
2023-09-20 13:37:21 +0400

    Initial Commit

---
+++ Dictionary/DictionaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  DictionaryApp.swift
+//  Dictionary
+//
+//  Created by Aleksey Ryabchenko on 20.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct DictionaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
